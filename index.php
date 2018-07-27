aae1173458a33f12c594b1c6aaea5cf58806f963
doukeshiDONALDO <b1401730@planet.kanazawa-it.ac.jp>
2018-07-27 20:01:52 +0900

    データベースを整形したので一度テーブル内をdeleteした後load dataしなおしてください

--- index.php
+++ index.php
@@ -4,11 +4,16 @@
 try {
 	$dbh = new PDO('mysql:host=localhost;dbname=parking;charset=utf8;', 'root', '',array(PDO::ATTR_PERSISTENT => true));
 	        foreach($dbh->query('SELECT * from parkings') as $row) {
-			        print_r($json = json_encode($row,JSON_UNESCAPED_UNICODE));
+			        print_r($json_data = json_encode($row,JSON_UNESCAPED_UNICODE));
 				    }
 	        $dbh = null;
 } catch (PDOException $e) {
 	    print "エラー!: " . $e->getMessage() . "<br/>";
 	        die();
 }
+?>
 
+<script>
+var test=JSON.parse('<?php echo  $json_data; ?>');
+
+</script>
